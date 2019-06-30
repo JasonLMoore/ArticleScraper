6c761f66f1b83b3d33c65e87fa7168c9daaf318b
Jason Moore <jason.moore1369@gmail.com>
2019-06-30 14:50:00 -0500

    server. js contains middleware, /articles and /articles/:id routes. PORT and mongoose.connect will need to be fixed

---
+++ server.js
@@ -0,0 +1,92 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+//scraping tools//
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+//req all models
+const db = require("./models");
+
+//PORT will need to change for Heroku//
+const PORT = 8080;
+//////////////////
+
+//init Express//
+const app = express();
+
+//MIDDLEWARE CONFIG//
+//morgan logger for logging requests (Taken from 18-20)//
+app.use(logger("dev"));
+//request body parsed as JSON//
+app.use(express.urlencoded({ extended:true }));
+app.use(express.json());
+//make public folder static//
+app.use(express.statuc("public"));
+
+//connect to the Mongo db//will need to change//
+mongoose.connect("mongodb://", { useNewUrlParser: true});
+////////////////////////////
+
+//ROUTES//
+
+//GET route for scraping NBA.com website//
+app.get("/scrape", function(req, res) {
+  //grab html body//
+  axios.get("https://www.nba.com/#/").then(function(response) {
+    //load into cherrio//save to $ for shorthand//
+    const $ = cherrio.load(response.data);
+
+    //grab every a within a content_list tag//
+    $("content_list a").each(function(i, element) {
+      const result = {};
+
+      //add text and href of every link, save as properties of result obj//
+      result.title = $(this)
+        .children("h5")
+        .text();
+      result.link = $(this)
+        .attr("href");
+      
+      //create new Article using `result` object from scraping//
+      db.Article.create(result)
+        .then(function(dbArticle) {
+          //view added result in console//
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+    });
+    
+    //send message to client//
+    res.send("Scrape Complete")
+  });
+});
+
+//Route for gettin all Articles from the db//
+app.get("/articles", function(req, res) {
+  //grab every doc in Articles collection//
+  db.Article.find({})
+    .then(function(dbArticle) {
+      //Success//send back to client//
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+//Route for grabbing specific Article by id, populate with comments//
+app.get("/articles/:id", function(req, res) {
+  db.Article.findOne({ _id: req.params.id })
+    .populate("comment")
+    .then(function(dbArticle) {
+      //Success//send back to client//
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
